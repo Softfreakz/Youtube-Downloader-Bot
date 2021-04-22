89cd5305873d1a8cc29355d9d581c44f2e8a3c8f
Softfreakz <65111266+Softfreakz@users.noreply.github.com>
2021-04-22 13:27:38 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,5 +3,5 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"No one can help you, except yourself!!! 😉 /n For any issues/support/error report contact my master Softfreakz!"
+    helptxt = f"No one can help you, except yourself! 😉 For any issues/support/error report contact my master Softfreakz!"
     await message.reply_text(helptxt)
